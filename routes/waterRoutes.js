c8fa7873033813d366918063d0f999183197c4bd
Quangmeo37 <144407744+Quangmeo37@users.noreply.github.com>
2024-11-26 14:06:07 +0700

    Delete routes directory

--- routes/waterRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const waterController = require('../controllers/waterController');
-
-// Route để lấy lịch sử dữ liệu mức nước
-router.get('/history', waterController.getWaterHistory);
-
-module.exports = router;
