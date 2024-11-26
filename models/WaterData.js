f3ee01db233b7c094743b02e8758f39a661f77d4
Quangmeo37 <144407744+Quangmeo37@users.noreply.github.com>
2024-11-26 14:04:16 +0700

    Delete models directory

--- models/WaterData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const waterSchema = new mongoose.Schema({
-  distance: Number,
-  pumpStatus: String,
-  timestamp: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('WaterData', waterSchema);
