cc3fad78ab771a97a26ed4b0c9fe77c93519a81a
Quangmeo37 <144407744+Quangmeo37@users.noreply.github.com>
2024-11-26 14:08:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,1 @@
-// config.js
 
-const config = {
-  // Cấu hình MongoDB
-  mongoURI: 'mongodb://localhost:27017/water_level_system',
-
-  // Cấu hình MQTT
-  mqttBrokerUrl: 'mqtts://quang007:123456789Aa@limesquash-9rmptg.a03.euc1.aws.hivemq.cloud:8883',
-  waterLevelTopic: 'sensor/water_level', // Topic nhận dữ liệu mực nước
-  pumpControlTopic: 'sensor/pump_control', // Topic gửi tín hiệu điều khiển máy bơm
-  deviceControlTopic: 'sensor/device_control', // Topic gửi tín hiệu điều khiển thiết bị khác
-
-  // Cấu hình cổng server
-  serverPort: 4000,
-};
-
-module.exports = config;
